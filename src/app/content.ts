85000e5a7d909c4d17a69ebc8d3506c047488bb4
Ralph Largo <devlargs@gmail.com>
2021-05-14 18:06:54 +0800

    initial commit

--- src/app/content.ts
+++ src/app/content.ts
@@ -1,8 +1,59 @@
 chrome.runtime.sendMessage({}, (response) => {
-    var checkReady = setInterval(() => {
-        if (document.readyState === "complete") {
-            clearInterval(checkReady)
-            console.log("We're in the injected content script!")
-        }
-    })
-})
+  const url = (a: string) =>
+    `https://www.signalclout.com/profile-analyzer?search=${a}&referrer=signalpop`;
+  const redirect = (name: string) => {
+    window.open(url(name), "_blank");
+  };
+
+  const checkReady = setInterval(() => {
+    if (document.readyState === "complete") {
+      clearInterval(checkReady);
+      const { host } = window.location;
+
+      if (host === "bitclout.com") {
+        let element;
+
+        const interval = setInterval(() => {
+          const a = document.getElementsByClassName(
+            "fs-24px font-weight-bold d-flex align-items-center"
+          )[1];
+
+          const { pathname } = window.location;
+
+          if (a && pathname.includes("/u/")) {
+            const name = pathname.split("/u/")[1];
+
+            element = a;
+            clearInterval(interval);
+            a.innerHTML = `@${name} 
+            <button id="signal">click</button> 
+          `;
+            document
+              .getElementById("signal")
+              .addEventListener("click", () => redirect(name));
+          }
+        }, 1000);
+      }
+
+      if (
+        host.includes("signalclout.com") ||
+        host.includes("signalclout.vercel.app")
+      ) {
+        console.log("nice one largs");
+
+        const interval = setInterval(() => {
+          const profile = document.querySelector(
+            'img[src="/search-signalclout-brand.svg"]'
+          );
+          console.log(profile);
+          if (profile) {
+            if (profile instanceof HTMLElement) {
+              profile.click();
+              clearInterval(interval);
+            }
+          }
+        }, 1000);
+      }
+    }
+  });
+});
